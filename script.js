09b941f3d40dc39bf30873283293e53add784468
Giovanni Luca Pinelli <giovannilucapinelli94@gmail.com>
2025-10-17 13:52:18 +0200

    Add files via upload
    * Redid the app's structure. The aim now is to make a section were saved up passwords are going to be put. I am trying to figure out how to do it all, but at the least I have an idea of how the project should end up as.
    * Since I had to change things around, CSS and JS might be a mess.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const passwordLength = document.getElementById("password-number");
 const passwordGen = document.getElementById("gen-btn");
-const passwordInput = document.getElementById("password-input");
-const copyButton = document.getElementById("copy-btn");
 const upperCaseCheck = document.getElementById("upper-case");
 const lowerCaseCheck = document.getElementById("lower-case");
 const numberCheck = document.getElementById("number");
@@ -9,104 +7,17 @@ const symbolCheck = document.getElementById("symbol");
 const avoidSame = document.getElementById("avoider");
 const avoidAmb = document.getElementById("ambiguous");
 const onlyHex = document.getElementById("hexa");
-const increase = document.getElementById("increase");
-const decrease = document.getElementById("decrease");
-const strengthRating = document.getElementById("passrating");
+const passGenerated = document.getElementById("passGenerated");
 
 passwordGen.addEventListener("click", passwordFunction);
-passwordLength.addEventListener("input", passwordFunction);
 
-upperCaseCheck.addEventListener("click", passwordFunction);
-lowerCaseCheck.addEventListener("click", passwordFunction);
-numberCheck.addEventListener("click", passwordFunction);
-symbolCheck.addEventListener("click", passwordFunction);
-avoidSame.addEventListener("click", passwordFunction);
+let numPass = document.getElementById("numPass");
 
-const copyModal = document.getElementById("copied-modal");
+numPass.textContent = passwordLength.value;
 
-let copyModalAppear;
-
-copyButton.addEventListener("click", () => {
-  navigator.clipboard.writeText(passwordInput.innerText);
-
-  copyModal.style.display = "flex";
-
-  if (copyModalAppear) {
-    return;
-  }
-  copyModalAppear = setTimeout(() => {
-    copyModal.style.display = "none";
-    copyModalAppear = null;
-  }, 2000);
+passwordLength.addEventListener("input", () => {
+  numPass.textContent = passwordLength.value;
 });
-
-increase.addEventListener("click", () => {
-  if (passwordLength.value >= 32) {
-    return;
-  }
-  passwordLength.value++;
-  passwordFunction();
-  passwordStrength();
-});
-
-decrease.addEventListener("click", () => {
-  if (passwordLength.value <= 0) {
-    return;
-  }
-  passwordLength.value--;
-  passwordFunction();
-  passwordStrength();
-});
-
-let increaseInterval;
-
-function startIncrease() {
-  if (increaseInterval) {
-    return;
-  }
-  increaseInterval = setInterval(() => {
-    if (passwordLength.value < 32) {
-      passwordLength.value++;
-      passwordFunction();
-      passwordStrength();
-    }
-  }, 100);
-}
-
-function stopIncrease() {
-  clearInterval(increaseInterval);
-  increaseInterval = null;
-}
-
-increase.addEventListener("touchstart", startIncrease);
-increase.addEventListener("touchend", stopIncrease);
-increase.addEventListener("mousedown", startIncrease);
-increase.addEventListener("mouseup", stopIncrease);
-
-let decreaseInterval
-
-function startDecrease() {
-  if (decreaseInterval) {
-    return;
-  }
-  decreaseInterval = setInterval(() => {
-    if (passwordLength.value > 0) {
-      passwordLength.value--;
-      passwordFunction();
-      passwordStrength();
-    }
-  }, 100);
-}
-
-function stopDecrease() {
-  clearInterval(decreaseInterval);
-  decreaseInterval = null;
-}
-
-decrease.addEventListener("touchstart", startDecrease);
-decrease.addEventListener("touchend", stopDecrease);
-decrease.addEventListener("mousedown", startDecrease);
-decrease.addEventListener("mouseup", stopDecrease);
 
 function passwordGenerator(length, upperCase, lowerCase, numbers, symbols) {
   let charSet = "";
@@ -137,13 +48,15 @@ function passwordGenerator(length, upperCase, lowerCase, numbers, symbols) {
   }
 
   if (onlyHex.checked) {
-    charSet = charSet.replace(/[GHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*\(\)\_\+\-\=\[\]\{\}\|\;\:\,\.\<\>\?]/g, "");
+    charSet = charSet.replace(
+      /[GHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*\(\)\_\+\-\=\[\]\{\}\|\;\:\,\.\<\>\?]/g,
+      ""
+    );
   }
 
   let password = "";
 
   let charArr = charSet.split("");
-
 
   for (let i = 0; i < length; i++) {
     if (avoidSame.checked) {
@@ -188,51 +101,112 @@ function passwordFunction() {
     symbols
   );
 
-  passwordInput.innerText = password;
+  passwordStorage(password);
+  passContainer(password);
+  
 }
 
-function passwordStrength() {
+function passContainer(password) {
+  let cont = document.createElement("div");
+  cont.classList.add("pass");
+
+  let para = document.createElement("p");
+  para.innerText = password;
+
+  let contB = document.createElement("div");
+  contB.classList.add("passB");
+
+  let del = document.createElement("button");
+  del.classList.add("delete-btn");
+  del.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`;
+
+  let copy = document.createElement("button");
+  copy.classList.add("copy-btn");
+  copy.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#e3e3e3"><path d="M360-240q-33 0-56.5-23.5T280-320v-480q0-33 23.5-56.5T360-880h360q33 0 56.5 23.5T800-800v480q0 33-23.5 56.5T720-240H360Zm0-80h360v-480H360v480ZM200-80q-33 0-56.5-23.5T120-160v-560h80v560h440v80H200Zm160-240v-480 480Z"/></svg>`;
+
+  cont.appendChild(para);
+  cont.appendChild(contB);
+  contB.appendChild(del);
+  contB.appendChild(copy);
+  passGenerated.appendChild(cont);
+}
+
+const copyModal = document.getElementById("copied-modal");
+
+let copyModalAppear;
+
+passGenerated.addEventListener("click", (e) => {
+  const del = e.target.closest(".delete-btn");
+  if (del) {
+    const p = del.closest(".pass");
+    if (p) {
+      p.remove();
+    }
+  }
+
+  const cop = e.target.closest(".copy-btn")
+  if (cop) {
+    const p = cop.closest(".pass");
+    
+    navigator.clipboard.writeText(p.innerText);
+
+    copyModal.style.display = "flex";
+
+    if (!copyModalAppear) {
+      copyModalAppear = setTimeout(() => {
+        copyModal.style.display = "none";
+        copyModalAppear = null;
+      }, 2000);
+    }
+  }
+  
+});
+
+const conts = passGenerated.querySelectorAll(".pass");
+for (let i = 0; i < conts.length; i++) {
+  if (i % 2 === 0) {
+    conts[i].style.backgroundColor = "red";
+  }
+}
+
+function passwordStrength(str) {
   let score = 0;
 
-  if (+passwordLength.value <= 8) {
+  if (pass.innerText.length <= 8) {
     score += 1;
-  } else if (+passwordLength.value <= 18) {
+  } else if (pass.innerText.length <= 18) {
     score += 2;
-  } else if (+passwordLength.value <= 24) {
+  } else if (pass.innerText.length <= 24) {
     score += 3;
   } else {
     score += 4;
   }
 
-  if (/[A-Z]/.test(passwordInput.innerText)) {
-    score += 1;
-  }
+  let charset = [];
 
-  if (/[a-z]/.test(passwordInput.innerText)) {
-    score += 1;
-  }
-
-  if (/[0-9]/.test(passwordInput.innerText)) {
-    score += 1;
-  }
-
-  if (/[!@#$%^&*()_\+\-=\[\]{}|;:,.<>?]/.test(passwordInput.innerText)) {
-    score += 1;
-  }
-
-  if (score <= 4) {
-    strengthRating.innerText = "BAD";
-  } else if (score <= 6) {
-    strengthRating.innerText = "GOOD!";
-  } else if (score === 8) {
-    strengthRating.innerText = "AMAZING!";
+  for (let char of pass.innerText) {
+    if (!charset.includes(char)) {
+      charset.push(char);
+      score += 1;
+    } else {
+      continue;
+    }
   }
 }
 
-upperCaseCheck.addEventListener("click", passwordStrength);
-lowerCaseCheck.addEventListener("click", passwordStrength);
-numberCheck.addEventListener("click", passwordStrength);
-symbolCheck.addEventListener("click", passwordStrength);
+function passwordStorage(p) {
+  const passwordSaved = localStorage.getItem("password");
+  const passwordList = passwordSaved ? JSON.parse(passwordSaved) : [];
 
-passwordStrength();
-passwordFunction();
+  passwordList.push(p);
+
+  localStorage.setItem("password", JSON.stringify(passwordList));
+}
+
+function loadPasswordStorage() {
+  const passwordList = JSON.parse(passwordSaved);
+
+  passwordList.forEach((p) => {});
+}
+
+loadPasswordStorage();
